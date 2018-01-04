e32e1385a269c05432517144bdb3507f7ac0efd1
Wouter Konecny <wouter@innovato.nl>
2018-01-04 16:07:11 +0100

    Removed unused code

--- src/containers/home/EventModal.js
+++ src/containers/home/EventModal.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { FormGroup, Button, ControlLabel, Modal } from 'react-bootstrap'
+import { FormGroup, FormControl, Button, ControlLabel, Modal } from 'react-bootstrap'
 import { connectModal, hide } from 'redux-modal';
 import Datetime from 'react-datetime';
 import Select from 'react-select';
 import moment from 'moment';
-import NumericInput from 'react-numeric-input';
+import NumericInput from "react-numeric-input";
 import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -20,8 +20,8 @@ class EventModal extends Component {
             start: moment(props.slotInfo.start),
             end: moment(props.slotInfo.end),
             km: (props.slotInfo.km !== undefined) ? props.slotInfo.km : 0,
+            notes: (props.slotInfo.notes !== undefined) ? props.slotInfo.notes : '',
         };
-        this.handleProjectChange = this.handleProjectChange.bind(this);
         this.handleSave = this.handleSave.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
         this.isNewEvent = this.isNewEvent.bind(this);
@@ -48,15 +48,20 @@ class EventModal extends Component {
         this.setState({ km: newKm });
     }
 
+    handleNotesChange = (event) => {
+        this.setState({ notes: event.target.value });
+    }
+
     handleSave() {
         const dataToSave = {
             title: this.state.project.name,
             start: this.state.start.toDate(),
             end: this.state.end.toDate(),
             project: this.state.project,
-            km: this.state.km
+            km: this.state.km,
+            notes: this.state.notes,
         };
-        if(this.isNewEvent()) {
+        if (this.isNewEvent()) {
             this.props.firestore.add('events', dataToSave);
         } else {
             this.props.firestore.update('events', this.props.slotInfo.id, dataToSave);
@@ -70,12 +75,12 @@ class EventModal extends Component {
     }
 
     isNewEvent() {
-        return this.props.slotInfo.project === undefined;
+        return this.props.slotInfo.id === undefined;
     }
 
     render() {
         const { show, handleHide, projects } = this.props
-        const isFormValid = this.state.start.isBefore(this.state.end) && this.state.project !== '' && this.state.km >= 0;
+        const isFormValid = this.state.start.isBefore(this.state.end) && this.state.project !== undefined && this.state.km >= 0;
         const addOrEdit = (this.isNewEvent()) ? 'Add' : 'Edit';
 
         return (
@@ -114,12 +119,21 @@ class EventModal extends Component {
                         <ControlLabel>Kilometer registration</ControlLabel>
                         <NumericInput min={0} value={this.state.km} onChange={this.handleKmChange} className="form-control" />
                     </FormGroup>
+                    <FormGroup>
+                        <ControlLabel>Notes</ControlLabel>
+                        <FormControl
+                            value={this.state.notes}
+                            onChange={this.handleNotesChange}
+                            className="form-control"
+                            componentClass="textarea"
+                            placeholder="Optional notes"
+                        />
+                    </FormGroup>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button onClick={handleHide}>Cancel</Button>
                     <Button bsStyle="primary" onClick={this.handleSave} disabled={!isFormValid}>Save</Button>
-
                     <span className="pull-left">
                         <Button bsStyle="danger" onClick={this.handleDelete} disabled={this.isNewEvent()}>Delete</Button>
                     </span>
