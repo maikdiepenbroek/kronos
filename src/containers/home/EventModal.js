80cc65d3f717d014e39872ea64bf8b30b601074b
Wouter Konecny <wouter@innovato.nl>
2018-01-04 13:04:06 +0100

    Added Kilometer registration to Event form

--- src/containers/home/EventModal.js
+++ src/containers/home/EventModal.js
@@ -4,17 +4,47 @@ import { Button, Modal } from 'react-bootstrap'
 import { connectModal } from 'redux-modal'
 import Datetime from 'react-datetime';
 import Select from 'react-select';
+import moment from 'moment';
+import NumericInput from "react-numeric-input";
 import 'react-select/dist/react-select.css';
 import 'react-datetime/css/react-datetime.css'
 
 class ModalContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projectId: '',
+            start: moment(props.modalData.slotInfo.start),
+            end: moment(props.modalData.slotInfo.end),
+            km: 0,
+        };
+        this.handleProjectChange = this.handleProjectChange.bind(this);
+    }
+
     static propTypes = {
         handleHide: PropTypes.func.isRequired,
         modalData: PropTypes.object.isRequired,
     };
+    
+    handleProjectChange = (selectedOption) => {
+        this.setState({ projectId: selectedOption });
+    }
+
+    handleStartChange = (moment) => {
+        this.setState({ start: moment });
+    }
+
+    handleEndChange = (moment) => {
+        this.setState({ end: moment });
+    }
+
+    handleKmChange = (newKm) => {
+        this.setState({ km: newKm });
+    }
 
     render() {
         const { show, handleHide, modalData } = this.props
+        const isFormValid = this.state.start.isBefore(this.state.end) && this.state.projectId != '' && this.state.km >= 0;
 
         return (
             <Modal show={show}>
@@ -24,22 +54,31 @@ class ModalContent extends Component {
 
                 <Modal.Body>
                     <p>Start:</p>
-                    <Datetime value={modalData.slotInfo.start} />
+                    <Datetime
+                        value={this.state.start}
+                        onChange={this.handleStartChange}
+                    />
                     <p>End:</p>
-                    <Datetime value={modalData.slotInfo.end} />
+                    <Datetime
+                        value={this.state.end}
+                        onChange={this.handleEndChange}
+                    />
                     <p>Project:</p>
                     <Select
                         name="project"
-                        onChange={this.handleChange}
+                        value={this.state.projectId}
+                        onChange={this.handleProjectChange}
                         valueKey="id"
                         labelKey="name"
                         options={modalData.projects}
                     />
+                    <p>Kilometer registration</p>
+                    <NumericInput min={0} value={this.state.km} onChange={this.handleKmChange} className="form-control"/>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button onClick={handleHide}>Cancel</Button>
-                    <Button bsStyle="primary" onClick={this.handleSave}>Save</Button>
+                    <Button bsStyle="primary" onClick={this.handleSave} disabled={!isFormValid}>Save</Button>
                 </Modal.Footer>
             </Modal>
         );
